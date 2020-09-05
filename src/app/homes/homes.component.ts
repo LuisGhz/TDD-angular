2507286413062d475adaddfa031e0fbb8af64016
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-09-05 12:57:07 -0600

    Comment hard code assigment. Add private to injected service.

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -11,27 +11,29 @@ export class HomesComponent implements OnInit {
 
   homes$;
 
-  constructor(dataService: DataService) { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
 
-    this.homes$ = of([
-      {
-        title: 'Home 1',
-        image: 'assets/listing.jpg',
-        location: 'new york'
-      },
-      {
-        title: 'Home 2',
-        image: 'assets/listing.jpg',
-        location: 'boston'
-      },
-      {
-        title: 'Home 3',
-        image: 'assets/listing.jpg',
-        location: 'chicago'
-      },
-    ])
+    this.homes$ = this.dataService.getHomes$();
+
+    // this.homes$ = of([
+    //   {
+    //     title: 'Home 1',
+    //     image: 'assets/listing.jpg',
+    //     location: 'new york'
+    //   },
+    //   {
+    //     title: 'Home 2',
+    //     image: 'assets/listing.jpg',
+    //     location: 'boston'
+    //   },
+    //   {
+    //     title: 'Home 3',
+    //     image: 'assets/listing.jpg',
+    //     location: 'chicago'
+    //   },
+    // ])
 
   }
 
