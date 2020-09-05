a28dd2569cf9d0b3bfbf2bc8211a985693dd0085
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-09-05 12:57:17 -0600

    Create getHomes$ method.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   constructor() { }
+
+  getHomes$() {
+    return of([]);
+  }
 }
