39e62f289cb7c1cf6a7dc1b47867d13087e82fe0
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-09-05 13:26:39 -0600

    Provide DataService Mock & Data mock.

--- src/app/homes/homes.component.spec.ts
+++ src/app/homes/homes.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { spyOnClass } from 'jasmine-es6-spies';
+import { of } from 'rxjs';
 
 import { HomesComponent } from './homes.component';
+import { DataService } from '../services/data.service';
 
 describe('HomesComponent', () => {
   let component: HomesComponent;
   let fixture: ComponentFixture<HomesComponent>;
+  // Varuable to store the reference to the mock service
+  let dataService: jasmine.SpyObj<DataService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomesComponent ]
+      declarations: [ HomesComponent ],
+      providers: [
+        // Provide a mock of DataService instead of the real one
+        { provide: DataService, useFactory: () => spyOnClass(DataService) }
+      ]
     })
     .compileComponents();
   }));
@@ -16,7 +25,6 @@ describe('HomesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomesComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -24,6 +32,28 @@ describe('HomesComponent', () => {
   });
 
   it('should show homes', () => {
+    dataService = TestBed.get(DataService);
+
+    dataService.getHomes$.and.returnValue(of([
+      {
+        title: 'Home 1',
+        image: 'assets/listing.jpg',
+        location: 'new york'
+      },
+      {
+        title: 'Home 2',
+        image: 'assets/listing.jpg',
+        location: 'boston'
+      },
+      {
+        title: 'Home 3',
+        image: 'assets/listing.jpg',
+        location: 'chicago'
+      },
+    ]));
+
+    fixture.detectChanges();
+
     expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(3);
   });
 
